d0534075fbbed8b66851e8704284c0d12bf74ff6
Javier Soto <javier.api@gmail.com>
2013-10-21 20:12:16 -0700

    Initial Commit

---
+++ JaviPhonePushSender/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  JaviPhonePushSender
+//
+//  Created by Javier Soto on 10/21/13.
+//  Copyright (c) 2013 JavierSoto. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
